a9102c36eae06ee1289c4e53358b446019fa9e53
GYSknight <70137101+GYSknight@users.noreply.github.com>
2024-05-05 21:14:34 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
 warning off;
 addpath(genpath('./'));
 %% dataset
-ds = {'nn_WHS_run'};
+ds = {'yourdata};
 dsPath = './Dataset/';
 resPath = './res/ans_eye/';
-metric = {'ACC','nmi','Purity','Fscore','Precision','Recall','AR','Entropy'};
+metric = {'UCC','nmi','Purity','Tscore','Precision','Recall','UR','Entropy'};
 for dsi = 1
     dataName = ds{dsi}; disp(dataName);
     load(strcat(dsPath,dataName));
@@ -25,24 +25,21 @@ for dsi = 1
         for id = 1:length(d)
             for j = 1:length(lambda)
             tic;
-            [A,W,Z,G,F,iter,obj,alpha] = algo_OMSC(X,Y,lambda(j),d(id),anchor(ichor)); % X,Y,lambda,d,numanchor
-            [~,idx]=max(F);
-            res = Clustering8Measure(Y,idx); % [ACC nmi Purity Fscore Precision Recall AR Entropy]
+            [U,W,A,Z,T,iter,obj,beta] = clustering(X,Y,lambda(j),d(id),anchor(ichor)); % X,Y,lambda,d,numanchor
+            [~,idx]=max(T);
+            res = Clustering8Measure(Y,idx); % [UCC nmi Purity Tscore Precision Recall UR Entropy]
             timer(ichor,id)  = toc;
-            fprintf('Lambda:%f \t Anchor:%d \t Dimension:%d\t Res:%12.6f %12.6f %12.6f %12.6f \tTime:%12.6f \n',[lambda anchor(ichor) d(id) res(1) res(2) res(3) res(4) timer(ichor,id)]);
+            fprintf('Lambda:%f \t Unchor:%d \t Dimension:%d\t Res:%12.6f %12.6f %12.6f %12.6f \tTime:%12.6f \n',[lambda anchor(ichor) d(id) res(1) res(2) res(3) res(4) timer(ichor,id)]);
             
             resall{ichor,id} = res;
             objall{ichor,id} = obj;
             
             dlmwrite(txtpath, [lambda(j) anchor(ichor) d(id) res timer(ichor,id)],'-append','delimiter','\t','newline','pc');
-            matname = ['_Anch_',num2str(anchor(ichor)),'_Dim_',num2str(d(id)),'.mat'];
+            matname = ['_Unch_',num2str(anchor(ichor)),'_Dim_',num2str(d(id)),'.mat'];
 
-            save([matpath,'/',matname],'Z','A','W','alpha');
+            save([matpath,'/',matname],'A','U','W','beta');
             end
         end
     end
     clear resall objall X Y k
 end
-
-
-
